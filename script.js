d6df29200883f11d6c0009a8a94a19fd5483150f
allegria25 <allegria.kinzola@2026.ulc-icam.com>
2023-06-02 13:30:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', ()=>{
+    shareLink();
+})
+
+
+function shareLink() {
+    if (navigator.share) {
+      navigator.share({
+        title: document.title,
+        url: location.href 
+      })
+      .then(() => console.log('Lien partagé avec succès'))
+      .catch((error) => console.error('Erreur lors du partage :', error));
+    } else {
+      console.log('API Web Share non prise en charge');
+    }
+  }
